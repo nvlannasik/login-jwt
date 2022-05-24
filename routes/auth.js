5e6d478776727c50adb3f201d33e34fa87b0a578
nvlannasik <annasik@yahoo.com>
2022-05-24 23:30:52 +0700

    add validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User.js");
+const { registerValidation } = require("../validation");
 
+//POST REGISTER
 router.post("/register", (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //Checking if user already exists
+  const emailExist = User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
   const { name, email, password } = req.body;
   const user = new User({
     name,
@@ -12,7 +21,7 @@ router.post("/register", (req, res) => {
     const savedUser = user.save();
     res.json(savedUser);
   } catch (err) {
-    res.json({ message: err });
+    res.status(400).send(err);
   }
 });
 module.exports = router;
